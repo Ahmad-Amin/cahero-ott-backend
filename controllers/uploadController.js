a45c3eb0d914705f65b9ae375226afc968e1431f
Ahmad-Amin <baigahmad323@gmail.com>
2024-10-13 03:39:15 +0500

    normalized the file paths accross all operating systems

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -5,7 +5,8 @@ const uploadFile = (req, res) => {
     return res.status(400).json({ message: 'No file uploaded' });
   }
 
-  const fileUrl = `${req.protocol}://${req.get('host')}/${req.file.path}`;
+  const normalizedPath = path.posix.normalize(req.file.path);
+  const fileUrl = `${req.protocol}://${req.get('host')}/${normalizedPath.replace(/\\/g, '/')}`;
 
   res.status(200).json({ message: 'File uploaded successfully', fileUrl });
 };
