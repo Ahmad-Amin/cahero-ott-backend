213b4242f772d02b8a9ee8fbb04f86b75fe5f55a
Ahmad-Amin <baigahmad323@gmail.com>
2024-10-19 12:24:39 +0500

    fornow stops the email sending, when the stream has started

--- controllers/webinarController.js
+++ controllers/webinarController.js
@@ -93,10 +93,10 @@ const webinarController = {
         return res.status(404).json({ message: 'Webinar not found' });
       }
 
-      // Check if the current user is authorized to update the webinar
-      if (webinar.user.toString() !== req.user.userId) {
-        return res.status(403).json({ message: 'Unauthorized: You cannot update this webinar' });
-      }
+      // // Check if the current user is authorized to update the webinar
+      // if (webinar.user.toString() !== req.user.userId) {
+      //   return res.status(403).json({ message: 'Unauthorized: You cannot update this webinar' });
+      // }
 
       const updatedWebinar = await Webinar.findByIdAndUpdate(
         webinarId,
@@ -178,32 +178,32 @@ const webinarController = {
       webinar.isLive = true;
       await webinar.save();
 
-      const users = await User.find({}, 'email');
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
+      // const users = await User.find({}, 'email');
+      // const transporter = nodemailer.createTransport({
+      //   service: 'gmail',
+      //   auth: {
+      //     user: process.env.EMAIL_USER,
+      //     pass: process.env.EMAIL_PASS,
+      //   },
+      // });
 
-      const mailOptions = {
-        from: process.env.EMAIL_USER,
-        subject: `${webinar.title} has started!`,
-        text: `The webinar "${webinar.title}" has just started!\n\n` +
-          `You can join the webinar using the following ID: **${webinar.id}**\n\n` +
-          `Details:\n` +
-          `Description: ${webinar.description}\n` +
-          `Date: ${webinar.startDate.toDateString()}\n` +
-          `Start Time: ${webinar.startTime}\n` +
-          `End Time: ${webinar.endTime}`
-      };
+      // const mailOptions = {
+      //   from: process.env.EMAIL_USER,
+      //   subject: `${webinar.title} has started!`,
+      //   text: `The webinar "${webinar.title}" has just started!\n\n` +
+      //     `You can join the webinar using the following ID: **${webinar.id}**\n\n` +
+      //     `Details:\n` +
+      //     `Description: ${webinar.description}\n` +
+      //     `Date: ${webinar.startDate.toDateString()}\n` +
+      //     `Start Time: ${webinar.startTime}\n` +
+      //     `End Time: ${webinar.endTime}`
+      // };
 
-      // Send email to all users
-      const emailPromises = users.map(user => {
-        return transporter.sendMail({ ...mailOptions, to: user.email });
-      });
-      await Promise.all(emailPromises);
+      // // Send email to all users
+      // const emailPromises = users.map(user => {
+      //   return transporter.sendMail({ ...mailOptions, to: user.email });
+      // });
+      // await Promise.all(emailPromises);
 
       res.status(200).json({ message: 'Streaming started & Emails sent successfully' });
     } catch (error) {
